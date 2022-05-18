1b65a35f4ef5f182531ee52f87399201c9b8def8
BuildTools <unconfigured@null.spigotmc.org>
2022-05-19 01:00:58 +0200

    plus jolie

--- cogs/classic/list.py
+++ cogs/classic/list.py
@@ -1,13 +1,13 @@
 from datetime import *
 from time import *
 from discord.ext import commands
-import sqlite3
-import asyncio
+import sqlite3 
+import asyncio #for timeout error
 
 sqlconnect = sqlite3.connect('./media/db/mlist.db')
 cursorsql = sqlconnect.cursor()
 
-def get_hight_id(table):
+def get_hight_id(table): #return new id for list
     max_id = cursorsql.execute(f"SELECT IDlist FROM {table} ORDER BY IDlist DESC LIMIT 1")
     max_id = max_id.fetchall()
     if len(max_id)==0:
@@ -35,14 +35,14 @@ class list(commands.Cog):
             await ctx.send("faut mettre un nom")
         else:
             if name_alrdy_take(name,id_admin):
-                error = await ctx.send(f"Vous avez déjà une table sous ce nom")
+                error = await ctx.send(f"Vous avez déjà une liste sous ce nom")
             else:
                 hight_id = get_hight_id("list")
                 iduseredit = []
                 type_list = 'private' #'public'
                 affiliation = 'user' #'server' 
                 cursorsql.execute("INSERT INTO list VALUES (?,?,?,?,?,?)",(hight_id,id_admin,f'{iduseredit}',f'{name}',type_list,affiliation))
-                creation_message = await ctx.send(f"Table {name} créé, cette list est en privé.")
+                creation_message = await ctx.send(f"Liste **{name}** créé.")
                 sqlconnect.commit()
 
     @commands.command(name="listdelete",aliases=['ld', 'listd', 'deletel'])
@@ -50,12 +50,12 @@ class list(commands.Cog):
         name = str(name[0])
         id_admin = ctx.author.id
         if not name:
-            await ctx.send("faut mettre un nom")
+            await ctx.send("Mettre un nom de liste [m!help]")
         else:
             if not name_alrdy_take(name,id_admin):
-                error = await ctx.send(f"Vous n'avez aucune table sous ce nom")
+                error = await ctx.send(f"Vous n'avez aucune liste sous ce nom.")
             else:
-                del_list_confirm = await ctx.send(f"Voulez vous vraiment supprimé la liste {name} ?")
+                del_list_confirm = await ctx.send(f"Voulez vous vraiment supprimé la liste **{name}** ?")
                 valid_reactions = [u"\u2705",u"\u274C"]
                 await del_list_confirm.add_reaction(valid_reactions[0])
                 await del_list_confirm.add_reaction(valid_reactions[1])
@@ -68,17 +68,17 @@ class list(commands.Cog):
                         cursorsql.execute("DELETE FROM list WHERE IDadmin = (?) and name = (?)",(id_admin,f'{name}'))
                         await del_list_confirm.clear_reaction(u"\u2705")
                         await del_list_confirm.clear_reaction(u"\u274C")
-                        await del_list_confirm.edit(content="List sup")
+                        await del_list_confirm.edit(content=f"Liste **{name}** supprimé.")
 
                     else:
                         await del_list_confirm.clear_reaction(u"\u2705")
                         await del_list_confirm.clear_reaction(u"\u274C")
-                        await del_list_confirm.edit(content="Cancelled")
+                        await del_list_confirm.edit(content="Annulé")
 
                 except asyncio.TimeoutError: 
                         await del_list_confirm.clear_reaction(u"\u2705")
                         await del_list_confirm.clear_reaction(u"\u274C")
-                        await del_list_confirm.edit(content="Cancelled [Timeout]")
+                        await del_list_confirm.edit(content="Annulé [Timeout]")
 
         
         sqlconnect.commit()
